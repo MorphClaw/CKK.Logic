19defb3f6b48a4de1ee222d2b855e76d0d6c9ae0
MorphClaw <97695240+MorphClaw@users.noreply.github.com>
2022-03-07 10:10:56 -0700

    Update Customer.cs

--- CKK.Logic/CKK.Logic/CKK.Logic/Models/Customer.cs
+++ CKK.Logic/CKK.Logic/CKK.Logic/Models/Customer.cs
@@ -11,6 +11,13 @@ namespace CKK.Logic.Models
         private int _id;
         private string _name;
         private string _address;
+        
+        public Customer(int id, string name, string address)
+        {
+            _id = id;
+            _name = name;
+            _address = address;
+        }
 
         public int GetId()
         {
