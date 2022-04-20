24a698aa3f9afc8bf90103644ac5d4d0f5d61cce
MorphClaw <97695240+MorphClaw@users.noreply.github.com>
2022-04-20 09:11:52 -0600

    Update Customer.cs

--- CKK.Logic/CKK.Logic/CKK.Logic/Models/Customer.cs
+++ CKK.Logic/CKK.Logic/CKK.Logic/Models/Customer.cs
@@ -1,51 +1,24 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CKK.Logic.Interfaces;
 
 namespace CKK.Logic.Models
 {
-    public class Customer
+    public class Customer : Entity
     {
-        private int _id;
-        private string _name;
         private string _address;
+        public string Address
+        {
+            get { return _address; }
+            set { _address = value; }
+        }
+        public Customer(string name = "default", int id = 0, string address = "default") : base(name, id)
+        {
+            _address = address;
+        }
         
-        public Customer(int id, string name, string address)
-        {
-            _id = id;
-            _name = name;
-            _address = address;
-        }
-
-        public int GetId()
-        {
-            return _id;
-        }
-        public void SetId(int id)
-        {
-            _id = id;
-        }
-
-        public string GetName()
-        {
-            return _name;
-        }
-
-        public void SetName(string name)
-        {
-            _name = name;
-        }
-
-        public string GetAddress()
-        {
-            return _address;
-        }
-
-        public void SetAddress(string address)
-        {
-            _address = address;
-        }
     }
 }
